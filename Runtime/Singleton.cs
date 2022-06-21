7c77ec4b9d8e986227e3a107e998197a00c1108d
Ant <a.cyprus.expbg@gmail.com>
2022-06-21 16:01:57 +0100

    Singleton: add non persistent instance property

--- Runtime/Singleton.cs
+++ Runtime/Singleton.cs
@@ -16,8 +16,9 @@ namespace UniEnt.Unity_QOL.Runtime {
 
         // ReSharper disable once MemberCanBeInternal
         /// <summary>
-        ///     Access singleton instance through this propriety.
+        ///     Access singleton instance through this property.
         /// </summary>
+        /// <remarks>This instance will persist across scene changes.</remarks>
         public static T Instance {
             get {
                 lock (SLock) {
@@ -38,6 +39,35 @@ namespace UniEnt.Unity_QOL.Runtime {
 
                     // Make instance persistent.
                     DontDestroyOnLoad(singletonObject);
+
+                    return _sInstance;
+                }
+            }
+        }
+
+
+        // ReSharper disable once MemberCanBeInternal
+        /// <summary>
+        ///     Access non persistent singleton instance through this property.
+        /// </summary>
+        /// <remarks>This instance will not persist across scene changes.</remarks>
+        public static T NonPersistentInstance {
+            get {
+                lock (SLock) {
+                    if (_sInstance != null)
+                        return _sInstance;
+
+                    // Search for existing instance.
+                    _sInstance = (T)FindObjectOfType(typeof(T));
+
+                    // Create new instance if one doesn't already exist.
+                    if (_sInstance != null)
+                        return _sInstance;
+
+                    // Need to create a new GameObject to attach the singleton to.
+                    var singletonObject = new GameObject();
+                    _sInstance = singletonObject.AddComponent<T>();
+                    singletonObject.name = typeof(T) + " (Singleton)";
 
                     return _sInstance;
                 }
